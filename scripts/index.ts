6cbf48e6b50f3b273e5d2d9a40176a832657cfd6
Mattia Franchetto <mt.franchetto@gmail.com>
2018-02-23 15:03:04 +0100

    Add auto dts

---
+++ scripts/index.ts
@@ -0,0 +1,7 @@
+export {default as AnalyticsModule} from "./AnalyticsModule";
+export {Page, Event} from "./TrackingDecorators";
+export {default as TrackPageRouteStrategy} from "./TrackPageRouteStrategy";
+export {default as IAnalyticsConfig} from "./interfaces/IAnalyticsConfig";
+export {default as IAnalyticsProvider} from "./interfaces/IAnalyticsProvider";
+export {ITrackingManager} from "./interfaces/ITrackingManager";
+export {default as TrackingCommandDispatcher} from "./TrackingCommandDispatcher";
